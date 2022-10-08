2b44af69d5fec0fa5c3e890f6b1e5b855f31d38c
phoenix <67361789+lordkilgore@users.noreply.github.com>
2022-10-07 20:32:18 -0700

    Update and rename windowdisplay to windowdisplay.py

---
+++ windowdisplay.py
@@ -0,0 +1,5 @@
+def windowDesign():
+    # toolbar box
+    pygame.draw.line(win, [0,0,0], (screenDimension -screenDimension, screenDimension-150), (screenDimension, screenDimension-150), 5)
+    pygame.draw.line(win, [0,0,0], (screenDimension -screenDimension, screenDimension-125), (screenDimension, screenDimension-125), 5)
+
