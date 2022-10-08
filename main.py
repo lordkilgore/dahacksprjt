100621aa9e4aae6234b2fffc712837962b06107b
Leonard <leonard.chandra04@gmail.com>
2022-10-07 23:20:17 -0700

    Update main.py

--- main.py
+++ main.py
@@ -8,6 +8,10 @@ win = pygame.display.set_mode((screenDimension, screenDimension))
 pygame.display.set_caption("Studies With Shaq")
 pygame.font.init()    
 
+def windowdisplay():
+    img = pygame.image.load('display.png')
+    win.blit(img, (0, screenDimension * 0.2))
+
 def refresh():
     win.fill([255, 255, 255])
 
