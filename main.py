47815f17634154031f729b0e5910a9ec5f815b53
phoenix <67361789+lordkilgore@users.noreply.github.com>
2022-10-07 20:23:17 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
+import pygame, math
+import time as tm
 
+pygame.display.init()                                                               
+
+screenDimension = int(pygame.display.Info().current_w * 0.5)                        
+win = pygame.display.set_mode((screenDimension, screenDimension))
+pygame.display.set_caption("Studies With Shaq")
+pygame.font.init()    
+
+def refresh():
+    win.fill([255, 255, 255])
+
+main = True
+while main:
+    for e in pygame.event.get():
+        if e.type == pygame.QUIT:
+            main = False
+
+    refresh()
+    pygame.display.update()
